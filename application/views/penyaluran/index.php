8345eb58caaebe869f0385375da4b2fc1350fa1e
noeadipratama <noe.adipratama@gmail.com>
2016-04-01 14:23:53 +0700

    rubah modal

--- application/views/penyaluran/index.php
+++ application/views/penyaluran/index.php
@@ -35,7 +35,7 @@
                             </button>
                             <!-- Modal -->
                             <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
+                                <div class="modal-dialog modal-lg">
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
